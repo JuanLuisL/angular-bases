9058e40f72ba71c0f85961879b140ee51b227391
m1jlariosm <jululama1@gmail.com>
2024-01-11 10:45:56 -0600

    Fin seccion 4

---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrl: './heroe.component.css'
+})
+export class HeroeComponent {
+
+  public name: string = 'Ironman';
+  public age:  number = 45;
+
+  get capitalizeName():string {
+    return this.name.toLocaleUpperCase();
+  }
+
+  getHeroDescription():string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeName():void {
+    this.name = 'Spiderman';
+  }
+
+  changeAge():void {
+    this.age = 25;
+  }
+
+  resetForm():void {
+    this.name = 'Ironman';
+    this.age = 45;
+  }
+}
