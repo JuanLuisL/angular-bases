709d8bcc74cc8c213b3c4e45b59751374663d044
m1jlariosm <jululama1@gmail.com>
2024-01-17 14:40:16 -0600

    Fin seccion 5

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -2,16 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { HeroeComponent } from "./heroe/heroe.component";
-import { ListComponent } from './list/list.component';
+
 
 @NgModule({
   declarations: [
     HeroeComponent,
-    ListComponent
   ],
   exports: [
     HeroeComponent,
-    ListComponent
   ],
   imports: [
     CommonModule
